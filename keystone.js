490c27a96a49cfe17c9f7f6f77a3f7c182c12f67
Henry Boston <henry.boston@gmail.com>
2016-10-06 17:28:20 -0500

    m

--- keystone.js
+++ keystone.js
@@ -102,6 +102,159 @@ keystone.set('nav', {
 	'links': ['links', 'link-tags', 'link-comments']
 });
 
+// app.js
+var discordbot = require('simple-discord-bot'),
+    authe = require('./auth.json');
+ 
+var subclubnum = 0;
 
+var commands = [{
+    name: 'stjern',
+    description: 'Stjerns Rogue Guide',
+    help: 'help for greet',
+    handle: stjern
+},
+{
+
+    name: 'simc',
+    description: 'Sim Yourself',
+    help: 'help for sim',
+    handle: simc,
+},
+{
+    name: 'stats',
+    description: 'Stats',
+    help: 'help for stats',
+    handle: stats,
+},
+{
+    name: 'followers',
+    description: 'Followers',
+    help: 'help for Followers',
+    handle: followers,
+},
+{
+    name: 'aethys',
+    description: 'Aethys',
+    help: 'help for aethys',
+    handle: aethys
+},
+{
+    name: 'channel',
+    description: 'channel',
+    help: 'help for channel',
+    handle: channel
+},
+{
+    name: 'artifact',
+    description: 'artifact',
+    help: 'help for artifact',
+    handle: artifact
+},
+{
+    name: 'enchants',
+    description: 'enchants',
+    help: 'help for enchants',
+    handle: enchants
+},
+{
+    name: 'brambles',
+    description: 'brambles',
+    help: 'help for mich',
+    handle: brambles
+},
+{
+    name: 'subclub',
+    description: 'subclub',
+    help: 'subclub',
+    handle: subclub
+},
+{
+    name: 'skasch',
+    description: 'skasch',
+    help: 'skasch',
+    handle: skasch
+},
+{
+    name: 'furple',
+    description: 'furple',
+    help: 'furple',
+    handle: furple
+},
+{
+    name: 'best',
+    description: 'best',
+    help: 'best',
+    handle: best
+},
+{
+    name: 'faq',
+    description: 'faq',
+    help: 'faq',
+    handle: faq
+}
+];
+ 
+function stjern (message) {
+    bot.sendMessage(message, '**Stjerns Rogue Guide: <https://goo.gl/c8ntLu>**');
+}
+
+function simc (message) {
+    bot.sendMessage(message, '**How To Sim Yourself: <https://goo.gl/YACUkz> Download SimulationCraft at https://simulationcraft.org/**');
+}
+
+function stats (message) {
+    bot.sendMessage(message, '**You should Sim yourself to find stat weights: <https://goo.gl/YACUkz>**');
+}
+
+function followers (message) {
+    bot.sendMessage(message, '**Follower Chart (not that it matters): <https://goo.gl/KwuHgF>**');
+}
+
+function aethys (message) {
+    bot.sendMessage(message, '**Aethys Updated Sim Results: <https://goo.gl/VSjNfw>**');
+}
+
+function channel (message) {
+    bot.sendMessage(message, 'That question should go in #simple-questions, #general, or #general-moderated');
+}
+
+function artifact (message) {
+    bot.sendMessage(message, 'Outlaw: <https://goo.gl/sdu2qo> Assassination: <https://goo.gl/p1c7wG> Subtlety MS: <https://goo.gl/6b4Y4Z> Subtlety OS: <https://goo.gl/KEJuZx>');
+}
+
+function enchants (message) {
+    bot.sendMessage(message, '**Q: What are the best enchants?** Neck: Mark of the Hidden Satyr, Cloak: Binding of Agility, Ring: Depends on spec and personal stat weights. Sim yourself.');
+}
+
+
+function brambles (message) {
+    bot.sendMessage(message, 'Brambles: the only reason rogues get invited to raid Emerald Nightmare');
+}
+
+function subclub (message) {
+	subclubnum = subclubnum + 1;
+    bot.sendMessage(message, " Currently " + subclubnum + " members in the !subclub '''⌐(ಠ۾ಠ)¬'''");
+}
+
+function skasch (message) {
+    bot.sendMessage(message, '**Skasch Sims:** <https://simc.skasch.com/>');
+}
+
+function furple (message) {
+    bot.sendMessage(message, 'I read on the internet I shouldnt trust Furple and he is an asshole <http://i.imgur.com/LhIHO0N.png> p.s. who is the real furple <http://i.imgur.com/dbvpeM2.png>' );
+}
+
+function best (message) {
+    bot.sendMessage(message, 'Reaves is dead I have replaced him. Go cry me a river.' );
+}
+
+function faq (message) {
+    bot.sendMessage(message, 'we have a #resources-and-faq channel https://i.imgflip.com/1azeky.jpg' );
+}
+ 
+var bot = new discordbot({ commands: commands });
+ 
+bot.connect(authe);
 
 keystone.start();
